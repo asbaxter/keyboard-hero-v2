2787f5cdf10eb66d7c2ad2ca5f522141d37a7c1c
Andrew Baxter <asbaxter1@gmail.com>
2022-09-07 16:08:13 -0400

    webpage now displays 4 different colored buttons and has event listeners

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,36 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const canvasWidth = canvas.width = 600;
+const canvasWidth = canvas.width = 525;
 const canvasHeight = canvas.height = 800;
 
 let notes = [];
 let noteIndex = 0;
+
+document.addEventListener("keydown", (event => {
+    let keyPressed = event.key;
+
+    if(keyPressed == 1){
+        console.log("button 1")
+    }
+    else if(keyPressed == 2){
+        console.log("button 2")
+    }
+    else if(keyPressed == 3){
+        console.log("button 3")
+    }
+    else if(keyPressed == 4){
+        console.log("button 4")
+    }
+    else {
+        return;
+    }
+}))
+
+document.addEventListener("keyup", (event => {
+    console.log('button up')
+}))
+
 
 class Note {
     constructor(id, note_x, note_y, color){
@@ -20,19 +45,19 @@ class Note {
         ctx.arc(this.note_x, this.note_y, 50, 0, 2 * Math.PI);
 
         if (this.color == 'green'){
-            ctx.fillStyle = '#228B22';
+            ctx.fillStyle = '#90EE90';
             ctx.fill();
         }
         else if (this.color == 'red'){
-            ctx.fillStyle = '#ff0000';
+            ctx.fillStyle = '#ffcccb';
             ctx.fill();
         }
         else if (this.color == 'yellow'){
-            ctx.fillStyle = '#FFFF00';
+            ctx.fillStyle = '#ffffe0';
             ctx.fill();
         }
         else{
-            ctx.fillStyle = '#0000FF';
+            ctx.fillStyle = '#ADD8E6';
             ctx.fill();
         }
         
@@ -56,7 +81,7 @@ function createNewNote(){
     let randNote = Math.floor(Math.random() * 4) + 1;
 
     if (randNote == 1){
-        const note = new Note(noteIndex, 100, 40, 'green');
+        const note = new Note(noteIndex, 75, 40, 'green');
         notes.push(note);
     }
     else if (randNote == 2){
@@ -64,11 +89,11 @@ function createNewNote(){
         notes.push(note);
     }
     else if (randNote == 3){
-        const note = new Note(noteIndex, 300, 40, 'yellow');
+        const note = new Note(noteIndex, 325, 40, 'yellow');
         notes.push(note);
     }
     else {
-        const note = new Note(noteIndex, 400, 40, 'blue');
+        const note = new Note(noteIndex, 450, 40, 'blue');
         notes.push(note);
     }
 
@@ -79,9 +104,29 @@ const interval = setInterval(function() {
   }, 500);
 
 
+function drawButtons(){
+
+    let lanePosition = 75;
+    let buttonColors = ['#228B22', '#ff0000', '#FFFF00', '#0000FF']
+
+    for (let i = 0; i < 4; i++){
+
+        ctx.beginPath();
+        ctx.arc(lanePosition, 725, 50, 0, 2 * Math.PI);
+        ctx.fillStyle = buttonColors[i];
+        ctx.fill();
+        ctx.stroke();
+        lanePosition = lanePosition + 125;
+    }
+}
+
+
+
+
 function update(){
     ctx.clearRect(0, 0, canvasWidth, canvasHeight)
 
+    drawButtons();
 
     for (let i = 0; i < notes.length; i++){
         notes[i].drawNote();

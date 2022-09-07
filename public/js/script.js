0b6d6161de0556008a2adcec3be7c84194f7193e
Andrew Baxter <asbaxter1@gmail.com>
2022-09-06 23:23:46 -0400

    notes are being created at set intervals and moving accross canvas

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,51 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-
 const canvasWidth = canvas.width = 600;
 const canvasHeight = canvas.height = 600;
 
-export class Note {
+let notes = [];
+
+class Note {
     constructor(id, note_x, note_y){
         this.id = id;
         this.note_x = note_x;
-        this.ntoe_y = note_y;
+        this.note_y = note_y;
     }
     drawNote(){
-        ctx.beginPath();
-        ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-        ctx.stroke();
 
+        ctx.beginPath();
+        ctx.arc(this.note_x, this.note_y, 50, 0, 2 * Math.PI);
+        ctx.stroke();
+        this.moveNote();
+    }
+    moveNote(){
+
+        this.note_y = this.note_y + 10;
+        console.log(this.note_y);
     }
 }
 
 function createNewNote(){
 
-    const note = new Note();
-    note.drawNote();
+    const note = new Note(1, 75, 40);
+    notes.push(note);
+
 }
+
+const interval = setInterval(function() {
+    createNewNote();
+  }, 2000);
+
 
 function update(){
     ctx.clearRect(0, 0, canvasWidth, canvasHeight)
 
 
-    createNewNote();
+    for (let i = 0; i < notes.length; i++){
+        notes[i].drawNote();
+    }
+
 
     requestAnimationFrame(update)
 }
